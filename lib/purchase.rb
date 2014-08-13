032e8941c682d83b0b5827eb1ffab79c523a6ba3
Carolyn Huynh & Amy Vaillancourt-Sals <student@epicodus.com>
2014-08-13 11:42:42 -0700

    add method for saving purchases to the database.

--- lib/purchase.rb
+++ lib/purchase.rb
@@ -18,4 +18,22 @@ class Purchase
     self.date_of_purchase == another_purchase.date_of_purchase
   end
 
+  def save
+    results = DB.exec("INSERT INTO purchases (amount, description, date_of_purchase) VALUES (#{@amount}, '#{@description}', '#{@date_of_purchase}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM purchases;")
+      purchases = []
+    results.each do |result|
+      id = result['id']
+      amount = result['amount'].to_i
+      description = result['description']
+      date_of_purchase = result['date_of_purchase']
+      purchase = Purchase.new({:id => id, :amount => amount, :description => description, :date_of_purchase => date_of_purchase})
+      purchases << purchase
+    end
+    purchases
+  end
 end
--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -3,11 +3,17 @@ require 'pry'
 
 describe Purchase do
   it 'adds a purchase to the database' do
-    test_purchase = Purchase.new({:amount => 599.00, :description => 'kiteboard', :date_of_purchase => '2014-11-07'})
+    test_purchase = Purchase.new({:amount => 599, :description => 'kiteboard', :date_of_purchase => '2014-11-07'})
     expect(test_purchase).to be_an_instance_of Purchase
-    expect(test_purchase.amount).to eq 599.00
+    expect(test_purchase.amount).to eq 599
     expect(test_purchase.description).to eq 'kiteboard'
     expect(test_purchase.date_of_purchase).to eq '2014-11-07'
+  end
+
+  it 'saves the purchases to the database' do
+    test_purchase = Purchase.new({:amount => 599, :description => 'kiteboard', :date_of_purchase => '2014-11-07'})
+    test_purchase.save
+    expect(Purchase.all).to eq [test_purchase]
   end
 
 end
